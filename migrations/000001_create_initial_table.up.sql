4367b6cbe759843d71f6f2adc0db0145dc292d69
Tao <tao.sun@zymego.com>
2023-09-04 13:09:06 +0200

    add test and update readme.md and complete

--- migrations/000001_create_initial_table.up.sql
+++ migrations/000001_create_initial_table.up.sql
@@ -1,17 +1,5 @@
 BEGIN;
 
-CREATE TABLE IF NOT EXISTS grade (
-    id SERIAL PRIMARY KEY,
-    school_id INT,
-    student_id INT,
-    course_id INT,
-    grade VARCHAR(2),
-    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMPTZ,
-    deleted_at TIMESTAMPTZ,
-    UNIQUE(school_id, student_id, course_id)
-);
-
 CREATE TABLE IF NOT EXISTS grade_scale (
     id SERIAL PRIMARY KEY,
     school_id INT,
@@ -23,5 +11,21 @@ CREATE TABLE IF NOT EXISTS grade_scale (
     deleted_at TIMESTAMPTZ,
     UNIQUE(school_id, grade)
 );
+
+CREATE TABLE IF NOT EXISTS grade (
+    id SERIAL PRIMARY KEY,
+    school_id INT,
+    student_id INT,
+    course_id INT,
+    grade VARCHAR(2),
+    created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMPTZ,
+    deleted_at TIMESTAMPTZ,
+    UNIQUE(school_id, student_id, course_id),
+    CONSTRAINT grade_grade_scale
+        FOREIGN KEY("school_id", "grade")
+        REFERENCES grade_scale("school_id", "grade")
+);
+
 
 COMMIT;
