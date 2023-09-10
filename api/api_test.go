7acd2066a71905eb6f139a7749a48dfd3d003a86
Tao <tao.sun@zymego.com>
2023-09-10 04:44:12 +0200

    apply table test to create and fetch student GPA

--- api/api_test.go
+++ api/api_test.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"bytes"
 	"encoding/json"
 	"log"
 	"net/http"
@@ -19,33 +20,125 @@ func TestHandleRetrieveStudentsGPA(t *testing.T) {
 	server := NewAPIServer(":8080", db)
 	db.ClearTables()
 	server.SeedDB("gradeScales.json", "grades.json")
-	t.Run("can fetch student gpa", func(t *testing.T) {
-		req, err := http.NewRequest(http.MethodGet, "/students/gpa", nil)
-		if err != nil {
-			t.Error(err)
-		}
-		res := httptest.NewRecorder()
-		err = server.handleRetrieveStudentsGPA(res, req)
-		if err != nil {
-			t.Error(err)
-		}
-		if res.Result().StatusCode != http.StatusOK {
-			t.Errorf("expected 200 ,but got %d", res.Result().StatusCode)
-		}
-		defer res.Result().Body.Close()
-		var expectedRst = []database.StudentGPA{
-			{SchoolID: 1, StudentID: 1, GPA: 2},
-			{SchoolID: 1, StudentID: 2, GPA: 2.4},
-			{SchoolID: 2, StudentID: 1, GPA: 3.6},
-			{SchoolID: 2, StudentID: 2, GPA: 2.3},
-			{SchoolID: 2, StudentID: 3, GPA: 1.9},
-		}
-		var gpaArr []database.StudentGPA
-		json.NewDecoder(res.Result().Body).Decode(&gpaArr)
 
-		if !reflect.DeepEqual(gpaArr, expectedRst) {
-			t.Errorf("expected %v, but got %v", expectedRst, gpaArr)
-		}
-	})
+	testCases := []struct {
+		name      string
+		gradeJson string
+		expected  []database.StudentGPA
+	}{
+		{
+			name:      "just load the initial GPA",
+			gradeJson: "",
+			expected: []database.StudentGPA{
+				{SchoolID: 1, StudentID: 1, GPA: 2},
+				{SchoolID: 1, StudentID: 2, GPA: 2.4},
+				{SchoolID: 2, StudentID: 1, GPA: 3.6},
+				{SchoolID: 2, StudentID: 2, GPA: 2.3},
+				{SchoolID: 2, StudentID: 3, GPA: 1.9},
+			},
+		},
+		{
+			name: "load the Grade",
+			gradeJson: `[
+				{
+					"school_id":  1,
+					"student_id": 4,
+					"course_id":  1,
+					"grade":     "A"
+				},
+				{
+					"school_id":  1,
+					"student_id": 4,
+					"course_id":  2,
+					"grade":     "D"
+				},
+				{
+					"school_id":  1,
+					"student_id": 4,
+					"course_id":  3,
+					"grade":     "B"
+				},
+				{
+					"school_id":  1,
+					"student_id": 4,
+					"course_id":  4,
+					"grade":     "B"
+				},
+				{
+					"school_id":  1,
+					"student_id": 4,
+					"course_id":  5,
+					"grade":     "D"
+				}
+			]`,
+			expected: []database.StudentGPA{
+				{SchoolID: 1, StudentID: 1, GPA: 2},
+				{SchoolID: 1, StudentID: 2, GPA: 2.4},
+				{SchoolID: 1, StudentID: 4, GPA: 2.4},
+				{SchoolID: 2, StudentID: 1, GPA: 3.6},
+				{SchoolID: 2, StudentID: 2, GPA: 2.3},
+				{SchoolID: 2, StudentID: 3, GPA: 1.9},
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			if tc.gradeJson != "" {
+				res, err := createStudentsGradeRequest(server, tc.gradeJson)
+				checkError(res, err, t)
+				res.Result().Body.Close()
+			}
+			res, err := retrieveStudentGPARequest(server)
+			checkError(res, err, t)
+			defer res.Result().Body.Close()
+
+			var gpaArr []database.StudentGPA
+			json.NewDecoder(res.Result().Body).Decode(&gpaArr)
+
+			if !reflect.DeepEqual(gpaArr, tc.expected) {
+				t.Errorf("expected %v, but got %v", tc.expected, gpaArr)
+			}
+		})
+	}
 	defer db.CloseConn()
+}
+
+func retrieveStudentGPARequest(server *APIServer) (*httptest.ResponseRecorder, error) {
+	req, err := http.NewRequest(http.MethodGet, "/students/gpa", nil)
+	if err != nil {
+		return nil, err
+	}
+	res := httptest.NewRecorder()
+	err = server.handleRetrieveStudentsGPA(res, req)
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
+
+func createStudentsGradeRequest(server *APIServer, payload string) (*httptest.ResponseRecorder, error) {
+	jsonBody := []byte(payload)
+	bodyReader := bytes.NewReader(jsonBody)
+	req, err := http.NewRequest(http.MethodPost, "/students/gpa", bodyReader)
+	if err != nil {
+		return nil, err
+	}
+	res := httptest.NewRecorder()
+	err = server.handleCreateStudentsGrade(res, req)
+	if err != nil {
+		return nil, err
+	}
+
+	return res, nil
+}
+
+func checkError(res *httptest.ResponseRecorder, err error, t *testing.T) {
+	if err != nil {
+		t.Error(err)
+	}
+	if res.Result().StatusCode != http.StatusOK {
+		t.Errorf("expected 200 ,but got %d", res.Result().StatusCode)
+	}
 }
